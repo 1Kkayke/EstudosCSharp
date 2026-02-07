a4ca3f6c3e84b9d1becf85e3611f182f0de3bb68
1Kkayke <kayke@users.noreply.github.com>
2026-02-07 10:53:07 -0400

    create a new methods, sub and sum to the calculator

--- calculator.cs
+++ calculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace HelloWorld
 {
@@ -7,39 +8,60 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-            string FinalResult = "Result:";
-            string num1 = string.Empty;
-            string num2 = string.Empty;
+            string FinalResult = "The Result of the sum is : "; 
             string typeOp = string.Empty;
-            
+
             Console.WriteLine("-------------- Welcome to my Calculator --------------- ");
             Console.WriteLine("1.sum");
             Console.WriteLine("2.Substraction");
             Console.WriteLine("3.Adiction");
             Console.WriteLine("4.Multiplication");
-            Console.WriteLine("--------------Digite sua operação--------------------");
+            Console.WriteLine("-------------- Type the operation --------------------");
             typeOp = Console.ReadLine();
             
             if (typeOp == "1")
             {
-                Console.WriteLine("--------------Soma---------------");
-                Console.WriteLine(" Digite o primeiro numero para somar");
-                num1 = Console.ReadLine();
-                Console.WriteLine("Digite o segundo digito para somar");
-                num2 = Console.ReadLine();
-
-                for (int x = 0; x < numbers.Length; x++) //percurr array of numbers
-                {
-                    if (x == numbers[x])
-                    {
-                        
-                    }
-                }
-
+                Console.WriteLine(FinalResult += Programa.sum("1"));
             }
+            else if (typeOp == "2")
+            {
+                Console.WriteLine(FinalResult += Programa.sub("2");
+            }
+            
         
         }
+
+        static string sum(string strings)
+        {
+            int num1;
+            int num2;
+            string finalResult;
+
+            Console.WriteLine("--------------Sum---------------");
+            Console.WriteLine("Type the first number to sum");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Type the second number to sum");
+            num2 = int.Parse(Console.ReadLine());
+
+            finalResult = ((num1 + num2).ToString());
+
+            return finalResult;
+
+        }
+
+        static string sub(string strings) {
+            int num1;
+            int num2;
+            string finalResult;
+
+            Console.WriteLine("---------Substraction-------");
+            Console.WriteLine("Type the first number to sub");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            finalResult = ((num1 + num2).ToString());
+
+            return finalResult;
+        } 
     }
 
 }
